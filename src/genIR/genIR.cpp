351e50d318a4c3f07ff3cd14702020b5ba31b566
Ruide <rdzhang@vt.edu>
2020-03-01 22:45:10 -0500

    add an example

--- src/genIR/genIR.cpp
+++ src/genIR/genIR.cpp
@@ -9,6 +9,15 @@
 #include <llvm/Bitcode/ReaderWriter.h>
 #include <llvm/Support/ToolOutputFile.h>
 #include <llvm/Support/FileSystem.h>
+#include <llvm/Support/Debug.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/Support/raw_os_ostream.h>
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/InstrTypes.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Type.h"
+
 
 using namespace llvm;
 
@@ -16,32 +25,46 @@ Module *makeLLVMModule(){
 	LLVMContext context; 
 	Module *mod = new Module("sum.ll", context);
 	mod -> setDataLayout("e-m:e-i64:64-f80:128-n8:16:32:64-S128");
-	// mod -> setDataLayout("e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-
-	// 	i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-
-	// 	v128:128:128-a0:0:64-s0:64:64-f80-128:128-
-	// 	n8:16:32:64-S128");
+	// mod->setDataLayout("e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:128:128-a0:0:64-s0:64:64-f80-128:128-n8:16:32:64-S128");
 	mod -> setTargetTriple("x86_64-unknown-linux-gnu");
+	// mod->setTargetTriple("x86_64-apple-macosx10.7.0");
 
-	SmallVector<Type*, 2> FuncTyArgs;
-	FuncTyArgs.push_back(IntegerType::get(mod->getContext(), 32));
-	FuncTyArgs.push_back(IntegerType::get(mod->getContext(), 32));
-	FunctionType *FuncTy = FunctionType::get(
-		/*Results*/ IntegerType::get(mod->getContext(), 32),
-		/*Params*/  FuncTyArgs, /*isVarArg=*/ false);
 
-	Function *funcSum = Function::Create(
-		/*Type=*/ FuncTy,
-		/*Linkage=*/ GlobalValue::ExternalLinkage,
-		/*Name=*/ "sum", mod);
-	funcSum -> setCallingConv(CallingConv::C);
+	Value *int32_a;
+	Value *int32_b;
+	//define i32 @main(i32 %argc, i8 **%argv)
+	Function *funcSum = cast<Function>(mod->
+	getOrInsertFunction("sum", IntegerType::getInt32Ty(mod->getContext()),
+	                    IntegerType::getInt32Ty(mod->getContext()),
+	                    IntegerType::getInt32Ty(mod->getContext()), NULL));
+	{
+		Function::arg_iterator args = funcSum->arg_begin();
+		int32_a = &*args++;
+		int32_a->setName("a");
+		int32_b = &*args++;
+		int32_b->setName("b");
+	}
 
-	Function::arg_iterator args = funcSum->arg_begin();
-	Value *int32_a = &(*args);
-	args++;
-	int32_a->setName("a");
-	Value *int32_b = &(*args);
-	args++;
-	int32_b->setName("b");
+	// SmallVector<Type*, 2> FuncTyArgs;
+	// FuncTyArgs.push_back(IntegerType::get(mod->getContext(), 32));
+	// FuncTyArgs.push_back(IntegerType::get(mod->getContext(), 32));
+	// FunctionType *FuncTy = FunctionType::get(
+	// 	/*Results*/ IntegerType::get(mod->getContext(), 32),
+	// 	/*Params*/  FuncTyArgs, /*isVarArg=*/ false);
+
+	// Function *funcSum = Function::Create(
+	// 	/*Type=*/ FuncTy,
+	// 	/*Linkage=*/ GlobalValue::ExternalLinkage,
+	// 	/*Name=*/ "sum", mod);
+	// funcSum -> setCallingConv(CallingConv::C);
+
+	// Function::arg_iterator args = funcSum->arg_begin();
+	// Value *int32_a = &(*args);
+	// args++;
+	// int32_a->setName("a");
+	// Value *int32_b = &(*args);
+	// args++;
+	// int32_b->setName("b");
 
 	BasicBlock *labelEntry = BasicBlock::Create(mod->getContext(), "entry", funcSum, 0);
 
@@ -66,47 +89,60 @@ Module *makeLLVMModule(){
 	return mod;
 }
 
-int main(){
-	LLVMContext context; 
-	outs() << "created context\n";
 
+
+
+int main(){
 	Module *Mod = makeLLVMModule();
 	outs() << "created module\n";
 
-	Mod->dump();
+	// Mod->dump();
 
-
-	raw_ostream *os;
-	verifyModule(*Mod,  os);
-	os->flush();
+	// raw_ostream *os;
+	// raw_ostream *out = &outs();
+	// verifyModule(*Mod,  out);
+	if (verifyModule(*Mod)) {
+		errs() << "Error: module failed verification.  This shouldn't happen.\n";
+		abort();
+	}
+	// outs();
+	
 	// bool verifyModule(const Module &M, raw_ostream *OS = nullptr,
 	//                  bool *BrokenDebugInfo = nullptr);
 	outs() << "verified module\n";
 
+	// Mod->dump();
+	//Get the output stream
+	raw_ostream *out = &outs();
+	std::error_code EC;
 
-	std::error_code error_code;
+	out = new raw_fd_ostream("./sum.bc", EC, sys::fs::F_None);
+	
 	// OwningPtr
-	std::unique_ptr<tool_output_file> Out(new tool_output_file("./sum.bc", error_code, sys::fs::F_None));
+	// std::unique_ptr<tool_output_file> Out(new tool_output_file("./sum.bc", EC, sys::fs::F_None));
 	//   tool_output_file(StringRef Filename, std::error_code &EC,
     //               sys::fs::OpenFlags Flags);
 
   	// tool_output_file(StringRef Filename, int FD);
-	outs() << "outputed file\n";
+	// outs() << "outputed file\n";
 
-	if(error_code){
-		errs() << '\n';
-		return -1;
-	}
+	// if(error_code){
+	// 	errs() << '\n';
+	// 	return -1;
+	// }
 
-	outs() << "checked error_code\n";
+	// outs() << "checked error_code\n";
 
 	// tool_output_file.os()
 	// raw_fd_ostream& llvm::tool_output_file::os
 	// Return the contained raw_fd_ostream.
 
-	Mod->dump();
+	// Mod->dump();
 
-	WriteBitcodeToFile(Mod, Out->os());
+	// WriteBitcodeToFile(Mod, Out->os());
+	outs() << *Mod;
+	WriteBitcodeToFile(Mod, *out);
+    // WriteBitcodeToFile(Mod.get(), *out);
 	//   void WriteBitcodeToFile(const Module *M, raw_ostream &Out,
                           // bool ShouldPreserveUseListOrder = false,
                           // const ModuleSummaryIndex *Index = nullptr,
@@ -115,6 +151,10 @@ int main(){
 
 	outs() << "writed Bitcode\n";
 
-	Out->keep(); // Declare success
+	//Clean up
+	if (out != &outs())
+		delete out;
+
+	// Out->keep(); // Declare success
 	return 0;
 }
